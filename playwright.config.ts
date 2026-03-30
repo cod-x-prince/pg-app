4798bad601eb3874484e7b90550959f5c861b23d
cod-x-prince <businessinfo1210@gmail.com>
2026-03-31 03:09:52 +0530

    ci: formatting cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
   //   use: { ...devices['Desktop Safari'] },
   // },
 
-    /* Test against mobile viewports. */
+  /* Test against mobile viewports. */
   // {
   //   name: 'Mobile Chrome',
   //   use: { ...devices['Pixel 5'] },
